fcf52f29cd795a1384fc3124f44d7cda55047a6f
kitark13 <kratykd@icloud.com>
2026-01-25 15:08:50 +0100

    nodejs hw02 v1

--- src/server.js
+++ src/server.js
@@ -1,51 +1,30 @@
 import 'dotenv/config';
 import express from 'express';
+import { logger } from './middleware/logger.js';
 import cors from 'cors';
-import pino from 'pino-http';
+
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+import NotesRoutes from './routes/notesRoutes.js';
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
 
-app.use(cors());
+app.use(logger);
 app.use(express.json());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(cors());
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
+app.use(NotesRoutes);
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
-});
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
+// denys_kratyk HFkAUCvJwgFjEmr0
